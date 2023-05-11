f52f466d573db47526d6e074d3a7df859789da0d
shyamkryadav <shyamkryadav345@gmail.com>
2023-05-11 21:22:44 +0545

    register page is ready

--- client/pages/register.js
+++ client/pages/register.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 function Register() {
   return (
-    <div>
-    
-    </div>
+            <>
+            <div className='RegisterBox'>
+            <h2>Register</h2>
+            <p>Fill Your Inforamtion</p>
+            <div className='inputBox'>
+               <form method='post'>
+               <input  type='text' placeholder="Please enter your name" required></input>
+                <input type='text'  placeholder="User name" required></input>
+                <input type='email'  placeholder="E-mail Address" required></input>
+                <input type='password'  placeholder="Password" required></input>
+                <button className='btn'>Create Account</button>
+               </form>
+            </div>
+            </div>
+            </>
   )
 }
 
